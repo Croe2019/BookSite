5f6983c9e3a933ecc6b413cbedb6cdcbc5bd0f8b
Croe2019 <sac77qbc@gmail.com>
2020-03-19 14:15:52 +0900

    デザイン部分修正

--- code/Laravel/resources/views/book_site/index.blade.php
+++ code/Laravel/resources/views/book_site/index.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 
     <title>Book Site</title>
   </head>
@@ -27,50 +27,94 @@
 
         <!-- トレンド又は、おすすめの本の一覧を表示
                 ここでは、データベースから取得した本のデータを表示し、詳細画面のURLを設置する(後でソースコードを修正) -->
-        <div class="card-group mx-auto mt-3 w-75">
-            <div class="card  border-primary">
-                <!-- この画像は、サンプルなので本の画像を取得したら本の画像に変更する -->
-                <img class="card-img-top" src="image/sample.jpg">
-                <div class="card-body">
-                    <h5 class="card-title">本のタイトル</h5>
-                    <p class="card-text">本の説明文を表示ここに本の説明部分を表示していく</p>
+        <div class="card-group mx-auto mt-3 mb-3 w-75">
+            <div class="row row-cols-2 row-cols-md-3">
+                <div class="col mb-4">
+                    <div class="card  border-primary">
+                        <!-- この画像は、サンプルなので本の画像を取得したら本の画像に変更する -->
+                        <a href="#" class="text-dark text-decoration-none">
+                            <img class="card-img-top" src="image/sample.jpg">
+                            <div class="card-body">
+                                <h5 class="card-title text-center">本のタイトル</h5>
+                            </div>
+                        </a>
+                        <div class="card-footer text-center">
+                            <small class="text-muted">○○分前に更新</small>
+                        </div>
+                    </div>
                 </div>
-                <div class="card-footer">
-                    <small class="text-muted">○○分前に更新</small>
-                </div>
-            </div>
 
-            <div class="card border-primary">
-                <!-- この画像は、サンプルなので本の画像を取得したら本の画像に変更する -->
-                <img class="card-img-top" src="image/sample.jpg">
-                <div class="card-body">
-                    <h5 class="card-title">本のタイトル2</h5>
-                    <p class="card-text">本の説明文を表示2別の本の説明部分を表示するようにする</p>
+                <div class="col mb-4">
+                    <div class="card  border-primary">
+                        <!-- この画像は、サンプルなので本の画像を取得したら本の画像に変更する -->
+                        <a href="#" class="text-dark text-decoration-none">
+                            <img class="card-img-top" src="image/sample.jpg">
+                            <div class="card-body">
+                                <h5 class="card-title text-center">本のタイトル</h5>
+                            </div>
+                        </a>
+                        <div class="card-footer text-center">
+                            <small class="text-muted">○○分前に更新</small>
+                        </div>
+                    </div>
                 </div>
-                <div class="card-footer">
-                    <small class="text-muted">○○分前に更新2</small>
-                </div>
-            </div>
 
-            <div class="card  border-primary">
-                <!-- この画像は、サンプルなので本の画像を取得したら本の画像に変更する -->
-                <img class="card-img-top" src="image/sample.jpg">
-                <div class="card-body">
-                    <h5 class="card-title">本のタイトル3</h5>
-                    <p class="card-text">本の説明文を表示3上記と同じようにして表示していく</p>
+                <div class="col mb-4">
+                    <div class="card  border-primary">
+                        <!-- この画像は、サンプルなので本の画像を取得したら本の画像に変更する -->
+                        <a href="#" class="text-dark text-decoration-none">
+                            <img class="card-img-top" src="image/sample.jpg">
+                            <div class="card-body">
+                                <h5 class="card-title text-center">本のタイトル</h5>
+                            </div>
+                        </a>
+                        <div class="card-footer text-center">
+                            <small class="text-muted">○○分前に更新</small>
+                        </div>
+                    </div>
                 </div>
-                <div class="card-footer">
-                    <small class="text-muted">○○分前に更新3</small>
+
+                <div class="col mb-4">
+                    <div class="card  border-primary">
+                        <!-- この画像は、サンプルなので本の画像を取得したら本の画像に変更する -->
+                        <a href="#" class="text-dark text-decoration-none">
+                            <img class="card-img-top" src="image/sample.jpg">
+                            <div class="card-body">
+                                <h5 class="card-title text-center">本のタイトル</h5>
+                            </div>
+                        </a>
+                        <div class="card-footer text-center">
+                            <small class="text-muted">○○分前に更新</small>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
 
+        <!-- ページネーションの数は、コントローラー側での処理と同じにする -->
+        <nav aria-label="...">
+            <ul class="pagination pagination-lg justify-content-center">
+                <li class="page-item">
+                    <a class="page-link" href="#" aria-label="前">
+                        <span aria-hidden="true">&laquo;</span>
+                    </a>
+                </li>
+                <li class="page-item"><a class="page-link" href="#">1</a></li>
+                <li class="page-item"><a class="page-link" href="#">2</a></li>
+                <li class="page-item"><a class="page-link" href="#">3</a></li>
+                <li class="page-item">
+                    <a class="page-link" href="#" aria-label="次">
+                        <span aria-hidden="true">&raquo;</span>
+                    </a>
+                </li>
+            </ul>
+        </nav>
     </div>
 
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
   </body>
 </html>
